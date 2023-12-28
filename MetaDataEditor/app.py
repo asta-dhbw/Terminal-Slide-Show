3e47c377b810ec671f2cbe0493fa75d42696deb4
Muddyblack <Muddyblack03@gmail.com>
2023-12-28 23:05:56 +0100

    Scroll Area for MetaDataEditor

--- MetaDataEditor/app.py
+++ MetaDataEditor/app.py
@@ -1,7 +1,8 @@
+""" Main GUI for editing images MetaData """
 import os
 import sys
 from datetime import datetime, timedelta
-from PyQt5.QtWidgets import (
+from PyQt5.QtWidgets import (  # pylint: disable=no-name-in-module
     QApplication,
     QWidget,
     QLabel,
@@ -9,12 +10,14 @@ from PyQt5.QtWidgets import (
     QVBoxLayout,
     QHBoxLayout,
     QTextEdit,
+    QScrollArea,
+    QSizePolicy,
     QFileDialog,
 )
 from PyQt5.QtCore import Qt, QEvent
 from PyQt5.QtGui import QPixmap, QIcon, QKeyEvent
 
-from MetaDataEditor.image_metadata_handler import read_metadata, write_metadata
+from image_metadata_handler import read_metadata, write_metadata
 
 
 # Constants
@@ -29,7 +32,8 @@ IMAGE_FORMATS = "Images (*.png *.jpg *.bmp)"
 
 
 def load_styles(self):
-    with open(STANDARD_STYLE_SHEET, "r") as file:
+    """Loads the styles from the styles.qss file"""
+    with open(STANDARD_STYLE_SHEET, "r", encoding="utf-8") as file:
         style = file.read()
         self.setStyleSheet(style)
 
@@ -40,12 +44,14 @@ class TagTextEdit(QTextEdit):
     """
 
     def keyPressEvent(self, event):
+        """Overwrite the keyPressEvent to allow tabbing between widgets"""
         if event.key() == Qt.Key_Tab and not event.modifiers():
             self.focusNextPrevChild(True)
         else:
             super().keyPressEvent(event)
 
     def event(self, event):
+        """Overwrite the event to allow tabbing between widgets"""
         if (
             event.type() == QEvent.KeyPress
             and event.key() == Qt.Key_Tab
@@ -58,6 +64,7 @@ class TagTextEdit(QTextEdit):
         return super().event(event)
 
     def focusInEvent(self, event):
+        """Overwrite the focusInEvent to select all text"""
         super().focusInEvent(event)
         self.selectAll()
 
@@ -101,10 +108,12 @@ class TagWidget(QWidget):
         self.setTabOrder(self.tag_name_edit, self.tag_value_edit)
 
     def set_values(self, tag_name, tag_value):
+        """Sets the values of the tag_name_edit and tag_value_edit"""
         self.tag_name_edit.setPlainText(tag_name)
         self.tag_value_edit.setPlainText(tag_value)
 
     def clear_fields(self):
+        """Clears the tag_name_edit and tag_value_edit"""
         try:
             self.tag_name_edit.clear()
             self.tag_value_edit.clear()
@@ -112,12 +121,14 @@ class TagWidget(QWidget):
             pass
 
     def remove_self(self):
+        """Removes itself from the parent layout"""
         try:
             self.deleteLater()
         except:
             pass
 
     def get_values(self):
+        """Returns the values of the tag_name_edit and tag_value_edit"""
         return self.tag_name_edit.toPlainText(), self.tag_value_edit.toPlainText()
 
 
@@ -127,6 +138,7 @@ class ImageEditorGUI(QWidget):
     """
 
     def __init__(self):
+        """Initialize the GUI"""
         super().__init__()
 
         self.setWindowIcon(QIcon(ICON_PATH))
@@ -136,6 +148,7 @@ class ImageEditorGUI(QWidget):
         self.initUI()
 
     def initUI(self):
+        """Initialize the UI"""
         self.image_label = QLabel(self)
         self.image_label.setAlignment(Qt.AlignCenter)
 
@@ -152,10 +165,23 @@ class ImageEditorGUI(QWidget):
         button_layout.addWidget(self.save_button)
         button_layout.setSpacing(15)
 
+        # Create a ScrollArea
+        self.scroll_area = QScrollArea(self)
+        self.scroll_area.setWidgetResizable(True)
+        self.scroll_area.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
+        # Create a widget for the ScrollArea
+        self.scroll_widget = QWidget()
+        self.scroll_area.setWidget(self.scroll_widget)
+
+        # Create a layout for the ScrollArea widget
+        self.scroll_layout = QVBoxLayout(self.scroll_widget)
+        self.scroll_layout.addWidget(self.image_label)
+        self.scroll_layout.addLayout(button_layout)
+
         # Add all to Mainlayout
         layout = QVBoxLayout()
-        layout.addWidget(self.image_label)
-        layout.addLayout(button_layout)
+        layout.addWidget(self.scroll_area)
 
         # Connect Buttons to functions
         self.add_button.clicked.connect(self.add_tag)
@@ -166,6 +192,7 @@ class ImageEditorGUI(QWidget):
         self.setLayout(layout)
 
     def update_window_title(self):
+        """Updates the window title to include the image name"""
         if self.image_path is not None:
             filename = os.path.basename(self.image_path)
             self.setWindowTitle(f"MetaData Editor ({filename})")
@@ -173,13 +200,14 @@ class ImageEditorGUI(QWidget):
             self.setWindowTitle("MetaData Editor")
 
     def add_tag(self):
+        """Adds a new TagWidget to the ScrollArea"""
         tag_widget = TagWidget()
         self.tag_widgets.append(tag_widget)
 
-        layout = self.layout()
-        layout.insertWidget(layout.count() - 1, tag_widget)
+        self.scroll_layout.addWidget(tag_widget)
 
     def add_standard_tag(self):
+        """Adds a new TagWidget with standard values to the ScrollArea"""
         # Create a TagWidget with standard values
         tag_widget = TagWidget(
             tag_name="STARTDATE", tag_value=CURRENT_DATE.strftime("%d.%m.%Y")
@@ -194,11 +222,11 @@ class ImageEditorGUI(QWidget):
         self.tag_widgets.append(tag_widget2)
 
         # Insert the new TagWidget before the last item (save_button)
-        layout = self.layout()
-        layout.insertWidget(layout.count() - 1, tag_widget)
-        layout.insertWidget(layout.count() - 1, tag_widget2)
+        self.scroll_layout.addWidget(tag_widget)
+        self.scroll_layout.addWidget(tag_widget2)
 
     def load_image(self):
+        """Opens a file dialog to select an image and adds it with metadata to the ScrollArea"""
         options = QFileDialog.Options()
         options |= QFileDialog.ReadOnly
         file_dialog = QFileDialog()
@@ -221,13 +249,12 @@ class ImageEditorGUI(QWidget):
             tag_widget.set_values(key, value)
             self.tag_widgets.append(tag_widget)
 
-            layout = self.layout()
-            layout.insertWidget(layout.count() - 1, tag_widget)
+            self.scroll_layout.addWidget(tag_widget)
 
     def save_image(self):
+        """Opens a file dialog to select a location to save the image with metadata"""
         if self.image_path is not None:
             options = QFileDialog.Options()
-            options |= QFileDialog.DontUseNativeDialog
             file_dialog = QFileDialog.getSaveFileName(
                 self, "Save Image", "", IMAGE_FORMATS, options=options
             )
@@ -242,6 +269,7 @@ class ImageEditorGUI(QWidget):
                 write_metadata(self.image_path, file_dialog[0], metadata_dict)
 
     def display_image(self, image_path):
+        """Displays the image in the image_label"""
         pixmap = QPixmap(image_path)
         pixmap = pixmap.scaledToWidth(self.image_label.width())
         self.image_label.setPixmap(pixmap)
