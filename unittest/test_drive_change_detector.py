032cf5cffc5a12e0d990b6036da66b30a88960e5
Muddyblack <Muddyblack03@gmail.com>
2023-12-30 13:24:09 +0100

    unittests and minor fixes

---
+++ unittest/test_drive_change_detector.py
@@ -0,0 +1,49 @@
+# pylint: disable=C
+# pylint: disable=unused-import
+import setup
+import json
+import unittest
+from unittest.mock import patch, MagicMock
+from googleapiclient.errors import HttpError
+import drive_change_detector as dcd
+
+
+class TestDriveChangeDetector(unittest.TestCase):
+    @patch(
+        "builtins.open",
+        new_callable=unittest.mock.mock_open,
+        read_data='{"key": "value"}',
+    )
+    def test_load_state(self, mock_open):
+        result = dcd.load_state("dummy_path")
+        self.assertEqual(result, {"key": "value"})
+        mock_open.assert_called_once_with("dummy_path", "r", encoding="utf-8")
+
+    @patch("builtins.open", new_callable=unittest.mock.mock_open)
+    @patch("json.dump")
+    def test_save_state(self, mock_dump, mock_open):
+        dcd.save_state({"key": "value"}, "dummy_path")
+        mock_open.assert_called_once_with("dummy_path", "w", encoding="utf-8")
+        mock_dump.assert_called_once_with({"key": "value"}, mock_open.return_value)
+
+    @patch("drive_change_detector.load_state", return_value={})
+    @patch("drive_change_detector.save_state")
+    def test_get_changes(self, mock_save_state, mock_load_state):
+        mock_service = MagicMock()
+        mock_service.files().list().execute.return_value = {
+            "files": [{"id": "file1", "name": "file1", "createdTime": "time1"}]
+        }
+
+        new_files, deleted_files = dcd.get_changes(mock_service, "dummy_dir_id")
+
+        mock_load_state.assert_called_once_with("./state.json")
+        assert mock_service.files().list.call_count == 2
+        mock_save_state.assert_called_once()
+        self.assertEqual(
+            new_files, [{"id": "file1", "name": "file1", "createdTime": "time1"}]
+        )
+        self.assertEqual(deleted_files, {})
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ unittest/test_drive_connector.py
@@ -0,0 +1,51 @@
+""" Unit tests for drive_connector.py """
+# pylint: disable=C
+# pylint: disable=unused-import
+
+import setup
+import unittest
+from unittest.mock import patch, MagicMock
+from google.auth.exceptions import GoogleAuthError
+from googleapiclient.errors import HttpError
+
+from drive_connector import connect_to_drive
+
+
+class TestDriveConnector(unittest.TestCase):
+    GOOGLE_API_ACCESS = "dummy.json"
+
+    def setUp(self):
+        self.mock_credentials = patch(
+            "drive_connector.service_account.Credentials.from_service_account_file"
+        ).start()
+        self.mock_build = patch("drive_connector.build").start()
+
+    def tearDown(self):
+        patch.stopall()
+
+    def test_connect_to_drive_success(self):
+        self.mock_credentials.return_value = MagicMock()
+        self.mock_build.return_value = MagicMock()
+        result = connect_to_drive(self.GOOGLE_API_ACCESS)
+        self.assertIsNotNone(result)
+
+    def test_connect_to_drive_auth_error(self):
+        self.mock_credentials.side_effect = GoogleAuthError("Auth error")
+        result = connect_to_drive(self.GOOGLE_API_ACCESS)
+        self.assertIsNone(result)
+
+    def test_connect_to_drive_http_error(self):
+        mock_response = MagicMock()
+        mock_response.reason = "Http error"
+        self.mock_build.side_effect = HttpError(mock_response, b"HttpError")
+        result = connect_to_drive(self.GOOGLE_API_ACCESS)
+        self.assertIsNone(result)
+
+    def test_connect_to_drive_generic_error(self):
+        self.mock_build.side_effect = Exception("Generic error")
+        result = connect_to_drive(self.GOOGLE_API_ACCESS)
+        self.assertIsNone(result)
+
+
+if __name__ == "__main__":
+    unittest.main()
