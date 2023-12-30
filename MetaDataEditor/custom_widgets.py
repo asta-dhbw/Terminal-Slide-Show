ab594170b1fd262fda1b55a77d112d118fd55987
Muddyblack <Muddyblack03@gmail.com>
2023-12-30 15:31:05 +0100

    restructured and tried tests for MetaDataEditor

---
+++ MetaDataEditor/custom_widgets.py
@@ -0,0 +1,104 @@
+""" This module contains custom widgets for the MetaDataEditor """
+from PyQt5.QtWidgets import (  # pylint: disable=no-name-in-module
+    QApplication,
+    QWidget,
+    QTextEdit,
+    QPushButton,
+    QHBoxLayout,
+)
+from PyQt5.QtCore import Qt, QEvent
+from PyQt5.QtGui import QKeyEvent
+
+
+class TagTextEdit(QTextEdit):
+    """
+    Custom QTextEdit that allows tabbing between widgets
+    """
+
+    def keyPressEvent(self, event):
+        """Overwrite the keyPressEvent to allow tabbing between widgets"""
+        if event.key() == Qt.Key_Tab and not event.modifiers():
+            self.focusNextPrevChild(True)
+        else:
+            super().keyPressEvent(event)
+
+    def event(self, event):
+        """Overwrite the event to allow tabbing between widgets"""
+        if (
+            event.type() == QEvent.KeyPress
+            and event.key() == Qt.Key_Tab
+            and not event.modifiers()
+        ):
+            QApplication.instance().sendEvent(
+                self.parent(), QKeyEvent(QEvent.KeyPress, Qt.Key_Tab, Qt.NoModifier)
+            )
+            return True
+        return super().event(event)
+
+    def focusInEvent(self, event):
+        """Overwrite the focusInEvent to select all text"""
+        super().focusInEvent(event)
+        self.selectAll()
+
+
+class TagWidget(QWidget):
+    """
+    Custom widget for editing a single tag
+    """
+
+    def __init__(self, tag_name="", tag_value="", text_field_height=50, parent=None):
+        super(TagWidget, self).__init__(parent)
+
+        self.TEXT_FIELD_HEIGHT = text_field_height
+
+        self.tag_name_edit = TagTextEdit(self)
+        if tag_name.strip() != "":
+            self.tag_name_edit.setPlainText(tag_name)
+        else:
+            self.tag_name_edit.setPlaceholderText("Tag Name")
+        self.tag_name_edit.setFixedHeight(self.TEXT_FIELD_HEIGHT)
+
+        self.tag_value_edit = TagTextEdit(self)
+        if tag_value.strip() != "":
+            self.tag_value_edit.setPlainText(tag_value)
+        else:
+            self.tag_value_edit.setPlaceholderText("Tag Value")
+        self.tag_value_edit.setFixedHeight(self.TEXT_FIELD_HEIGHT)
+
+        self.remove_button = QPushButton("Remove", self)
+        self.remove_button.setObjectName("removeButton")
+
+        tag_layout = QHBoxLayout()
+        tag_layout.addWidget(self.tag_name_edit)
+        tag_layout.addWidget(self.tag_value_edit)
+        tag_layout.addWidget(self.remove_button)
+        tag_layout.setSpacing(10)
+
+        self.setLayout(tag_layout)
+
+        self.remove_button.clicked.connect(self.remove_self)
+        self.setTabOrder(self.tag_name_edit, self.tag_value_edit)
+
+    def set_values(self, tag_name, tag_value):
+        """Sets the values of the tag_name_edit and tag_value_edit"""
+        self.tag_name_edit.setPlainText(tag_name)
+        self.tag_value_edit.setPlainText(tag_value)
+
+    def clear_fields(self):
+        """Clears the tag_name_edit and tag_value_edit"""
+        try:
+            self.tag_name_edit.clear()
+            self.tag_value_edit.clear()
+        except:
+            pass
+
+    def remove_self(self):
+        """Removes itself from the parent layout"""
+        try:
+            self.deleteLater()
+        except:
+            pass
+
+    def get_values(self):
+        """Returns the values of the tag_name_edit and tag_value_edit"""
+        return self.tag_name_edit.toPlainText(), self.tag_value_edit.toPlainText()
---
+++ MetaDataEditor/unittests/test_custom_widgets.py
@@ -0,0 +1,70 @@
+# pylint: disable=C
+# pylint: disable=unused-import
+# pylint: disable=pointless-string-statement
+import __init__
+import unittest
+from PyQt5.QtWidgets import QApplication
+from custom_widgets import TagWidget, TagTextEdit
+from PyQt5.QtGui import QKeyEvent, QFocusEvent
+from PyQt5.QtCore import Qt, QEvent
+
+
+class TestTagWidget(unittest.TestCase):
+    def setUp(self):
+        self.app = QApplication([])
+
+    def tearDown(self):
+        self.app.quit()
+
+    def test_set_values(self):
+        widget = TagWidget()
+        widget.set_values("TagName", "TagValue")
+        self.assertEqual(widget.tag_name_edit.toPlainText(), "TagName")
+        self.assertEqual(widget.tag_value_edit.toPlainText(), "TagValue")
+
+    def test_clear_fields(self):
+        widget = TagWidget()
+        widget.set_values("TagName", "TagValue")
+        widget.clear_fields()
+        self.assertEqual(widget.tag_name_edit.toPlainText(), "")
+        self.assertEqual(widget.tag_value_edit.toPlainText(), "")
+
+    def test_remove_self(self):
+        widget = TagWidget()
+        parent_layout = widget.parent().layout() if widget.parent() else None
+        widget.remove_self()
+        self.assertIsNone(parent_layout)
+
+    def test_get_values(self):
+        widget = TagWidget()
+        widget.set_values("TagName", "TagValue")
+        result = widget.get_values()
+        self.assertEqual(result, ("TagName", "TagValue"))
+
+
+#!!! This test is not working
+"""
+class TestTagTextEdit(unittest.TestCase):
+    def setUp(self):
+        self.app = QApplication([])
+
+    def tearDown(self):
+        self.app.quit()
+
+    def test_tab_key_press_event(self):
+        text_edit = TagTextEdit()
+        event = QKeyEvent(QEvent.KeyPress, Qt.Key_Tab, Qt.NoModifier)
+        text_edit.keyPressEvent(event)
+        # Add assertions based on the expected behavior of Tab key press event
+
+    def test_focus_in_event(self):
+        text_edit = TagTextEdit()
+        # Simulate a focus in event
+        text_edit.focusInEvent(QFocusEvent(QEvent.FocusIn))
+        # Add assertions based on the expected behavior of focus in event
+
+    # Add more tests as needed
+"""
+
+if __name__ == "__main__":
+    unittest.main()
