a677bb32d68f02f9c77f1981e280e01ac1d2bc05
Christian <Muddyblack03@gmail.com>
2024-10-31 10:31:39 +0100

    fix oneday check

--- server/src/utils/fileValidator.js
+++ server/src/utils/fileValidator.js
@@ -18,5 +18,13 @@ export function isValidFile(filename) {
     return false; // End date is in the past
     }
 
+    // If there is no end date, ensure the start date is today
+    if (!endDate) {
+        const startDateString = startDate.toISOString().split('T')[0];
+        const nowString = now.toISOString().split('T')[0];
+        return startDateString === nowString;
+    }
+
+
     return true;
 }
