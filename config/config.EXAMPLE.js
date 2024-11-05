626abfda4ae5315819f56e797a5b3f116733a333
Christian <Muddyblack03@gmail.com>
2024-11-05 20:05:41 +0100

    config update

--- config/config.EXAMPLE.js
+++ config/config.EXAMPLE.js
@@ -1,9 +1,9 @@
 export const config = {
   info: {
-    location: 'CITY',
+    location: 'LOCATION',
   },
   apiKeys: {
-    NASA_API_KEY: 'KEY'  //https://api.nasa.gov/
+    NASA_API_KEY: 'API' // https://api.nasa.gov/
   },
   google: {
     serviceAccountPath: './config/service-account.json', // Path to Google Drive service account file
@@ -12,20 +12,20 @@ export const config = {
     apiVersion: 'v3' // Google Drive API version
   },
   backendPowerSaving: {
-    timeout: 5 * 60 * 1000 // 5 minutes in milliseconds
+    timeout: 5 * 60 * 1000 // standard 5 minutes in milliseconds
   },
   sync: {
     interval: 1000, // Default sync interval in milliseconds
     controlsInterval: 3000, // Interval for showing/hiding controls in milliseconds
     retryAttempts: 3, // Number of retry attempts for failed syncs
-    retryDelay: 1000 // Delay between retry attempts in milliseconds
+    retryDelay: 1 * 60 * 1000, // Delay between retry attempts in milliseconds
   },
   paths: {
     downloadPath: './downloads', // Path to download files from Google Drive
     archivePath: './archive', // Path to archive old files
   },
   logging: {
-    level: 'info', // Logging level (debug, info, warn, error)
+    level: 'debug', // Logging level (debug, info, warn, error)
     format: 'json' // Logging format (text, json)
   },
   backend: {
@@ -51,13 +51,13 @@ export const config = {
     offTime: '20:00', // Time to turn off the slideshow (24-hour format)
     days: [1, 2, 3, 4, 5], // Days to run the slideshow (0 for Sunday, 1 for Monday, ..., 6 for Saturday)
     vacationPeriods: [
-      { start: '24.06.2024', end: '24.07.2024' }, // Example vacation period
+      { start: '23.12.2024', end: '06.01.2025' }, // Example vacation period
     ],
   },
   mediaTypes: {
     imageTypes: ['.jpg', '.jpeg', '.png', '.gif'], // Supported image types
     videoTypes: ['.mp4', '.webm', '.ogg'], // Supported video types
-    loop: false, // Loop media files
+    loop: true, // Loop media files
     autoplay: true, // Autoplay media files
     muted: true // Mute media files
   }
